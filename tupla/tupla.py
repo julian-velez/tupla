25e8f79c5c85507142772f57a9c4273b8179eaaa
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-01-06 11:51:01 -0500

    Add files via upload
    tupla in python

---
+++ tupla/tupla.py
@@ -0,0 +1,7 @@
+# Se define una variable llamada "numeros" y se le asigna una tupla.
+# La tupla contiene dos valores: -10 y -10.
+numeros = (-10, -10)
+
+# La función print imprime el contenido de la variable "numeros".
+# Al ejecutarse, mostrará la tupla (-10, -10) en la consola.
+print(numeros)
